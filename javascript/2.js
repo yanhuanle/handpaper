a29b058e78921b2e027703aebdbcaecd6e41d928
yanhuanle <yanhuanle@baidu.com>
2022-05-18 12:03:41 +0800

    更新文档

--- javascript/2.js
+++
@@ -1,7 +0,0 @@
-const flatten = array => {
-    return array.reduce((acc, cur) => {
-        return Array.isArray(cur)
-            ? [...acc, ...flatten(cur)]
-            : [...acc, cur]
-    }, [])
-}
---
+++ 算法与数据结构/全排列.js
@@ -0,0 +1,28 @@
+let fullPermutation = (nums) => {
+    let res = [];
+    let used = {};
+
+    function dfs(path = []) {
+        if (path.length === nums.length) {
+            res.push(path.slice());
+            return;
+        }
+
+        for (let num of nums) {
+            if (used[num]) {
+                continue;
+            }
+            path.push(num);
+            used[num] = true;
+            dfs(path);
+            path.pop();
+            used[num] = false;
+        }
+    }
+
+    dfs();
+    return res;
+};
+
+let arr = ['1', '2', '3']
+console.log(fullPermutation(arr))
